796803b91868cf29baadcb0c0c0bab737a3024ad
Ghufran Ahmed <Hashmi>
2014-05-08 16:41:27 +0500

    new controller added

---
+++ ci_gah/application/controllers/main.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Maim extends CI_Controller {
+    public function index() {
+        $this->load->view('maincontroller');
+    }
+}
+
+/* End of file welcome.php */
